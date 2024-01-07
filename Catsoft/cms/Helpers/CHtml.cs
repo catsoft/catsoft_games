1f01f66144af6ccbd25489e6fbf68793413d53b7
Aleksandr Vakhtin <alexandr@tari.com>
2024-01-07 00:22:46 +0000

    multylangeuage

---
+++ Catsoft/cms/Helpers/CHtml.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace App.cms.Helpers
+{
+    public static class CHtml
+    {
+        public static Task<IHtmlContent> RenderText(this IHtmlHelper helper, string tag)
+        {
+            return helper.PartialAsync("/cms/Views/Shared/Text.cshtml", tag, null);
+        }
+    }
+}
