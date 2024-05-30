1ea1750bc50a7f8fc4b1bce0a841921b4da5e228
Aleksandr Vakhtin <alexandr@tari.com>
2024-05-30 22:27:26 +0100

    date

--- Catsoft/Models/Booking/AppointTimeModel.cs
+++ Catsoft/Models/Booking/AppointTimeModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using App.cms.Controllers.Attributes;
 using App.cms.Models;
 using App.cms.Options;
+using App.cms.StaticHelpers.Cookies;
+using Microsoft.AspNetCore.Http;
 
 namespace App.Models.Booking
 {
@@ -34,9 +37,20 @@ namespace App.Models.Booking
 
         public bool Blocked { get; set; }
 
-        public string FormattedShortDate()
+        public string FormattedShortDate(HttpContext context)
         {
-            return Date.ToString("M");
+            return Date.ToString("M", GetCulturalInfo(context));
+        }
+
+        public string FormatLongDate(HttpContext context)
+        {
+            return Date.ToString("dddd, dd MMMM", GetCulturalInfo(context));
+        }
+
+        private CultureInfo GetCulturalInfo(HttpContext context)
+        {
+            var language = CookieHelper.GetLanguage(context);
+            return new CultureInfo(language.GetCulturalCode());
         }
         
         public string FormattedPrice()
