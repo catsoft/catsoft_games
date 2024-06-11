9accb1faa2e3258a3f4f82083762bd55a03742c9
Aleksandr Vakhtin <alexandr@tari.com>
2024-06-11 14:03:23 +0100

    refactoring

---
+++ Catsoft/ViewModels/Booking/Time/BookingTimeItemDto.cs
@@ -0,0 +1,19 @@
+﻿namespace App.ViewModels.Booking.Time
+{
+    public class BookingTimeItemDto
+    {
+        public AppointTimeDto AppointTimeDto { get; set; }
+
+        public bool Selected { get; set; }
+
+        public int Places { get; set; } = 0;
+        
+        public BookingTimeItemType BookingTimeItemType { get; set; } = BookingTimeItemType.Date;
+    }
+
+    public enum BookingTimeItemType
+    {
+        Places,
+        Date,
+    }
+}
