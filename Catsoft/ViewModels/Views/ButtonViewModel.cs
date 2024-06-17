e4df747a6c8843d5fcfab80f24457fa5fdf0f3ca
Aleksandr Vakhtin <alexandr@tari.com>
2024-06-17 17:19:14 +0100

    justify content

--- Catsoft/ViewModels/Views/ButtonViewModel.cs
+++ Catsoft/ViewModels/Views/ButtonViewModel.cs
@@ -1,8 +1,9 @@
 ﻿namespace App.ViewModels.Views
 {
-    public class LinkButtonViewModel(string labelTag, string action, string controller) {
+    public class ButtonViewModel(string labelTag, string action, string controller, string method) {
         public string LabelTag { get; } = labelTag;
         public string Action { get; } = action;
         public string Controller { get; } = controller;
+        public string Method { get; } = method;
     }
 }
