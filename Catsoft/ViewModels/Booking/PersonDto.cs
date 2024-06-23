1e9d789ff1c67670f5602cd3942b9a5a2050c4df
Alexandr <theworldtocollapse@mail.ru>
2024-06-23 20:07:07 +0100

    input refactoring

--- Catsoft/ViewModels/Booking/PersonDto.cs
+++ Catsoft/ViewModels/Booking/PersonDto.cs
@@ -1,9 +1,24 @@
 ﻿using App.Models.Booking;
+using App.ViewModels.Views;
 
 namespace App.ViewModels.Booking
 {
     public class PersonDto(PersonModel personModel)
     {
         public PersonModel PersonModel { get; set; } = personModel;
+
+        public static string IsCompanyClassName = "company_data";
+
+        public bool Enabled { get; set; } = true;
+        
+        public InputViewModel GetPhoneInput() => new("Phone", "Phone", PersonModel.Phone, Enabled);
+        public InputViewModel GetEmailInput() => new("Email", "Email", PersonModel.Email, Enabled);
+        public InputViewModel GetNifInput() => new("NIF (optional)", "NIF", PersonModel.NIF, Enabled);
+        public InputViewModel GetNameInput() => new("Name", "Name", PersonModel.FullName, Enabled);
+        public InputViewModel GetCommentInput() => new("Comment (optional)", "Comment", PersonModel.Comment, Enabled);
+        public InputViewModel GetCompanyNif() => new("Company NIF", "CompanyNIF", PersonModel.CompanyNIF, Enabled);
+        public InputViewModel GetCompanyName() => new("Company Name", "CompanyName", PersonModel.CompanyName, Enabled);
+        public InputViewModel GetCompanyAddress() => new("Company Address", "CompanyAddress", PersonModel.CompanyAddress, Enabled);
+        public CheckBoxControlViewModel GetIsCompany() => new("Is company", "IsCompany", PersonModel.IsCompany, IsCompanyClassName, Enabled);
     }
 }
